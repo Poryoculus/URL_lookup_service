f92fa57f05f2450fc1e2c6b430ffbb69e68462c3
Poryoculus <rodrigopachecojor@gmail.com>
2026-01-23 11:59:30 -0300

    Get endpoint
    I added the Get endpoint in the main.py

--- src/main.py
+++ src/main.py
@@ -6,3 +6,17 @@ app = FastAPI()
 
 def health():
     return {"status": "ok"}
+
+
+@app.get("/urlinfo/1/{hostname_and_port}/{original_path:path}")
+
+def url_info(hostname_and_port: str, original_path: str):
+    full_url = f"http://{hostname_and_port}/{original_path}"
+
+    return {
+        "hostname_and_port": hostname_and_port,
+        "path": original_path,
+        "full_url": full_url,
+        "is_malware": False,
+        "safe": True,
+    }
